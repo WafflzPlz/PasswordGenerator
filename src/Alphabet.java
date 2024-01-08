9022f9d45af87f7b3051fa7efc08ed50a8be220e
WafflzPlz <michael.kogler1@gmx.at>
2024-01-08 18:03:30 +0100

    implemented Alphabet pool

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -1,6 +1,24 @@
 public class Alphabet {
-    private final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
-    private final String NUMBERS = "0123456789";
-    private final String SIGNS = "!§$%&/()=?+#*@{[]}-";
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBERS = "0123456789";
+    private static final String SIGNS = "!§$%&/()=?+#*@{[]}-";
+
+    private static StringBuilder pool;
+
+    public static String getPool(boolean upper, boolean lower, boolean num, boolean sign) {
+        if (upper) {
+            pool.append(UPPERCASE);
+        }
+        if (lower) {
+            pool.append(LOWERCASE);
+        }
+        if (num) {
+            pool.append(NUMBERS);
+        }
+        if (sign) {
+            pool.append(SIGNS);
+        }
+        return pool.toString();
+    }
 }
