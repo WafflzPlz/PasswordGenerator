babf73e3560ca06d9cb5af048d2985c70d022422
WafflzPlz <michael.kogler1@gmx.at>
2024-01-08 17:50:52 +0100

    switch to linux

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -3,8 +3,4 @@ public class Alphabet {
     private final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private final String NUMBERS = "0123456789";
     private final String SIGNS = "!§$%&/()=?+#*@{[]}-";
-
-    public String getUppercaseLetter(int i) {
-        return String.valueOf(UPPERCASE.charAt(i));
-    }
 }
