91be86f58a907e1280bd0d34f36923073ad504d6
WafflzPlz <michael.kogler1@gmx.at>
2024-01-08 18:18:09 +0100

    added initial setup

--- src/Generator.java
+++ src/Generator.java
@@ -1,17 +1,21 @@
 public class Generator {
 
-    private final Alphabet alphabet = new Alphabet();
-
     private boolean useUpper = false;
     private boolean useLower = false;
     private boolean useNumbers = false;
     private boolean useSigns = false;
 
+    private String pool;
     public Generator(boolean upper, boolean lower, boolean numbers, boolean signs) {
         useUpper = upper;
         useLower = lower;
         useNumbers = numbers;
         useSigns = signs;
+        pool = Alphabet.getAlphabet(upper, lower, numbers, signs);
+    }
+
+    public String generatePassword() {
+        return null;
     }
 
 }
